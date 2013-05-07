2c3c043b509aa2429eb780fb059c13144143fe69
zamgo <bitcoin.webskin@gmail.com>
2013-05-07 14:00:09 +0300

    test generic generation

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
-// PHPCoinAddress test - Version 0.1.0
+// PHPCoinAddress test - Version 0.1.1
+
+print "PHPCoinAddress Test:\n";
 
 require_once('PHPCoinAddress.php');
 
@@ -17,5 +19,11 @@ $coin = CoinAddress::litecoin_testnet();print 'LITECOIN-T: ' . $coin['private'] 
 
 $coin = CoinAddress::ppcoin();          print 'PPCOIN    : ' . $coin['private'] . ' ' . $coin['public'] . "\n";
 $coin = CoinAddress::ppcoin_testnet();  print 'PPCOIN-T  : ' . $coin['private'] . ' ' . $coin['public'] . "\n";
+
+
+$public_prefix = '0x42';
+$private_prefix = '0xAB';
+$coin = CoinAddress::generic( $public_prefix, $private_prefix);
+print "GENERIC pub:$public_prefix priv:$private_prefix : " . $coin['private'] . ' ' . $coin['public'] . "\n";
 
 exit;
