6374c23381acd05c894c70052d5c382d1a438268
zamgo <bitcoin.webskin@gmail.com>
2013-05-10 13:10:01 +0300

    add coins

--- test.php
+++ test.php
@@ -1,7 +1,8 @@
 <?php
-// PHPCoinAddress test - Version 0.1.8
+// PHPCoinAddress test 
+$version = '0.1.9.pre'
 
-print "\nPHPCoinAddress Test:\n";
+print "\nTEST: PHPCoinAddress $version\n";
 
 require_once('PHPCoinAddress.php');
 CoinAddress::set_debug(false);
@@ -13,39 +14,42 @@ print "Debug: " . ( CoinAddress::$debug? 'true' : 'false' ) . "\n";
 
 $start = microtime(1);
 
+// START TEST
+
 $coin = CoinAddress::bitcoin();          coin_info('Bitcoin', $coin);
+$coin = CoinAddress::bbqcoin();          coin_info('BBQcoin', $coin);
+$coin = CoinAddress::bitbar();           coin_info('Bitbar', $coin);
 $coin = CoinAddress::bytecoin();         coin_info('Bytecoin', $coin);
 $coin = CoinAddress::chncoin();          coin_info('CHNcoin', $coin);
 $coin = CoinAddress::devcoin();          coin_info('Devcoin', $coin);
+//$coin = CoinAddress::fairbrix();         coin_info('Fairbrix', $coin);
 $coin = CoinAddress::feathercoin();      coin_info('Feathercoin', $coin);
 $coin = CoinAddress::freicoin();         coin_info('Freicoin', $coin);
+//$coin = CoinAddress::ixcoin();           coin_info('IXcoin', $coin);
 $coin = CoinAddress::junkcoin();         coin_info('Junkcoin', $coin);
 $coin = CoinAddress::litecoin();         coin_info('Litecoin', $coin);
+$coin = CoinAddress::mincoin();          coin_info('Mincoin', $coin);
 $coin = CoinAddress::namecoin();         coin_info('Namecoin', $coin);
 $coin = CoinAddress::novacoin();         coin_info('Novacoin', $coin);
+$coin = CoinAddress::onecoin();          coin_info('Onecoin', $coin);
 $coin = CoinAddress::ppcoin();           coin_info('PPCoin', $coin);
+//$coin = CoinAddress::royalcoin();        coin_info('Royalcoin', $coin);
+$coin = CoinAddress::smallchange();      coin_info('Smallchange', $coin);
+$coin = CoinAddress::terracoin();        coin_info('Terracoin', $coin);
 $coin = CoinAddress::yacoin();           coin_info('Yacoin', $coin);
 
 
 $coin = CoinAddress::bitcoin_testnet();     coin_info('Bitcoin Testnet', $coin);
-//$coin = CoinAddress::bytecoin_testnet();    coin_info('Bytecoin Testnet', $coin);
-//$coin = CoinAddress::chncoin_testnet();     coin_info('CHNcoin Testnet', $coin);
-//$coin = CoinAddress::devcoin_testnet();     coin_info('Devcoin Testnet', $coin);
-//$coin = CoinAddress::feathercoin_testnet(); coin_info('Feathercoin Testnet', $coin);
-//$coin = CoinAddress::freicoin_testnet();    coin_info('Freicoin Testnet', $coin);
-//$coin = CoinAddress::junkcoin_testnet();    coin_info('Junkcoin Testnet', $coin);
-//$coin = CoinAddress::litecoin_testnet();    coin_info('Litecoin Testnet', $coin);
-//$coin = CoinAddress::namecoin_testnet();    coin_info('Namecoin Testnet', $coin);
-//$coin = CoinAddress::novacoin_testnet();    coin_info('Novacoin Testnet', $coin);
-//$coin = CoinAddress::ppcoin_testnet();      coin_info('PPCoin Testnet', $coin);
-//$coin = CoinAddress::yacoin_testnet();      coin_info('Yacoin Testnet', $coin);
-
+$coin = CoinAddress::bbqcoin_testnet();     coin_info('BBQcoin Testnet', $coin);
+$coin = CoinAddress::bitbar_testnet();     coin_info('Bitbar Testnet', $coin);
+// all other coin testnets uses Bitcoin prefixes
 
 $public_prefix  = '0x' . dechex( mt_rand(0,255) );
 $private_prefix = '0x' . dechex( mt_rand(0,255) );
 $coin = CoinAddress::generic( $public_prefix, $private_prefix);  
-coin_info("[Generic: public_prefix: $public_prefix  private_prefix: $private_prefix]", $coin);
+coin_info("[Random: public_prefix: $public_prefix  private_prefix: $private_prefix]", $coin);
 
+// END TEST
 
 $end = microtime(1);
 $duration = $end - $start;
